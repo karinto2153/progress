5843c050712bd6825fe667e322f872742e40fada
高月裕太 <takay2153@gmail.com>
2017-02-22 13:52:13 +0900

    sinatra lessons finished

--- main.rb
+++ main.rb
@@ -1,15 +1,30 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection(
+    "adapter" => "sqlite3",
+    "database" => "./bbs.db"
+)
+
+helpers do
+    include Rack::Utils
+    alias_method :h, :escape_html
+end
+
+class Comment < ActiveRecord::Base
+end
 
 get '/' do
-  @title = "main index"
-  @content = "main content"
+  @comments = Comment.order("id desc").all
   erb :index
 end
 
-get '/about' do
-  @title = "about this page"
-  @content = "this page is ..."
-  @email = "taguchi@gmail.com"
-  erb :about
+post '/new' do
+  Comment.create({:body => params[:body]})
+  redirect'/'
+end
+
+post '/delete' do
+  Comment.find(params[:id]).destroy
 end
